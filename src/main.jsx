761e1508f069267c0e09208c362eea5220b0cfdb
zhengyh <1827789327@qq.com>
2024-06-28 15:56:35 +0800

    antd中文化

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import Router from './layouts/Router';
 import './assets/style/index.scss';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
 
+dayjs.locale('zh-cn'); // 日期类组件设置中文
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ConfigProvider locale={zhCN}>
         <Router />
